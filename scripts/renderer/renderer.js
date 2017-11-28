f99c274ca313e9e1e2a40043b8b77997e119f73b
krysmathis <krysmathis@gmail.com>
2017-11-28 14:02:27 -0600

    added basic renderer function

--- scripts/renderer/renderer.js
+++ scripts/renderer/renderer.js
@@ -1,6 +1,6 @@
 const $ = require("jquery");
 
-data = {
+const data = [{
     "name": "Bourne Identify",
     "releaseDate": "2001-12-01",
     "actors": null,
@@ -10,7 +10,51 @@ data = {
     "rating": 5,
     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
     "overview": "In a world..."
-}
+},
+{
+    "name": "Bourne Identify",
+    "releaseDate": "2001-12-01",
+    "actors": null,
+    "id": 2501,
+    "watchlist": true,
+    "watched": true,
+    "rating": 5,
+    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+    "overview": "In a world..."
+},
+{
+    "name": "Bourne Identify",
+    "releaseDate": "2001-12-01",
+    "actors": null,
+    "id": 2501,
+    "watchlist": true,
+    "watched": true,
+    "rating": 5,
+    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+    "overview": "In a world..."
+},
+{
+    "name": "Bourne Identify",
+    "releaseDate": "2001-12-01",
+    "actors": null,
+    "id": 2501,
+    "watchlist": true,
+    "watched": true,
+    "rating": 5,
+    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+    "overview": "In a world..."
+},
+{
+    "name": "Bourne Identify",
+    "releaseDate": "2001-12-01",
+    "actors": null,
+    "id": 2501,
+    "watchlist": true,
+    "watched": true,
+    "rating": 5,
+    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+    "overview": "In a world..."
+}];
 
 const Renderer = Object.create(null, {
    
@@ -18,30 +62,40 @@ const Renderer = Object.create(null, {
         value: function(data, appendToElement) {
             //for each element in the array
             //create a new element and append it to the array
+            // clear the array to start fresh
             $(appendToElement).html("");
-            $row = $("div")
-            $row.addClass("row");
+            /**
+             * Materialize uses rows to handle it's grid
+             */
+            let row = document.createElement("div");
+            row.className = "row";
 
-            const posterPath = `http://image.tmdb.org/t/p/w300${data.poster_path}`
+            let cardContainer = document.createElement("div");
+            cardContainer.className = "col s12 cards-container";
+            row.appendChild(cardContainer);
+
+            // loop through the data array
             data.forEach(movie => {
-                
-                if 
-                
-                $row.append(
-                    `<div class="card" id="movie!${movie.id}">
-                        <div class="card-image">
-                            <img src="${movie.posterPath}">
-                            <span class="card-title">${movie.name}</span>
+                const posterPath = `http://image.tmdb.org/t/p/w300${movie.posterPath}`;
+
+                cardContainer.innerHTML +=
+                    `<div class="col m4">
+                        <div class="card" id="movie!${movie.id}">
+                            <div class="card-image">
+                                <img src="${posterPath}">
+                                <span class="card-title">${movie.name}</span>
+                            </div>
+                            <div class="card-content">
+                            <p>${movie.overview}</p>
+                            </div>
+                            <div class="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
                         </div>
-                        <div class="card-content">
-                        <p>${movie.overview}</p>
-                        </div>
-                        <div class="card-action">
-                        <a href="#">This is a link</a>
-                        </div>
-                    </div>`)
-            })
-           
+                    </div>`;
+            });
+            appendToElement.append(row);
+
         },
         enumerable: true
     },
@@ -52,4 +106,6 @@ const Renderer = Object.create(null, {
         enumerable: true
     }
 });
+
+Renderer.all(data,$(".article__image"));
 

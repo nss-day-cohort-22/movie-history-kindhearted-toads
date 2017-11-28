f3d6407bbd7c40e12942805e7290f2aaff8bdab6
krysmathis <krysmathis@gmail.com>
2017-11-28 16:12:32 -0600

    initial commit of renderer

--- scripts/renderer/renderer.js
+++ scripts/renderer/renderer.js
@@ -1,104 +1,66 @@
-const $ = require("jquery");
-
-const data = [{
-    "name": "Bourne Identify",
-    "releaseDate": "2001-12-01",
-    "actors": null,
-    "id": 2501,
-    "watchlist": true,
-    "watched": true,
-    "rating": 5,
-    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
-    "overview": "In a world..."
-},
-{
-    "name": "Bourne Identify",
-    "releaseDate": "2001-12-01",
-    "actors": null,
-    "id": 2501,
-    "watchlist": true,
-    "watched": true,
-    "rating": 5,
-    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
-    "overview": "In a world..."
-},
-{
-    "name": "Bourne Identify",
-    "releaseDate": "2001-12-01",
-    "actors": null,
-    "id": 2501,
-    "watchlist": true,
-    "watched": true,
-    "rating": 5,
-    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
-    "overview": "In a world..."
-},
-{
-    "name": "Bourne Identify",
-    "releaseDate": "2001-12-01",
-    "actors": null,
-    "id": 2501,
-    "watchlist": true,
-    "watched": true,
-    "rating": 5,
-    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
-    "overview": "In a world..."
-},
-{
-    "name": "Bourne Identify",
-    "releaseDate": "2001-12-01",
-    "actors": null,
-    "id": 2501,
-    "watchlist": true,
-    "watched": true,
-    "rating": 5,
-    "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
-    "overview": "In a world..."
-}];
-
 const Renderer = Object.create(null, {
    
-    "all": {
-        value: function(data, appendToElement) {
+    "append": {
+        value: function(movie, el) {
             //for each element in the array
             //create a new element and append it to the array
-            // clear the array to start fresh
-            $(appendToElement).html("");
-            /**
-             * Materialize uses rows to handle it's grid
-             */
-            let row = document.createElement("div");
-            row.className = "row";
+            let appendToElement = $(`.${el}`)
 
-            let cardContainer = document.createElement("div");
-            cardContainer.className = "col s12 cards-container";
-            row.appendChild(cardContainer);
-
+            let cardContainer = $("div");
+            cardContainer.className = "col m4"
+            
+            let actionDiv = "";
             // loop through the data array
-            data.forEach(movie => {
-                const posterPath = `http://image.tmdb.org/t/p/w300${movie.posterPath}`;
 
-                cardContainer.innerHTML +=
-                    `<div class="col m4">
-                        <div class="card" id="movie!${movie.id}">
+            // create the string for the rating
+            let rating = movie.rating;
+            let ratingString = "";
+            for (let i = 0; i < 5; i++) {
+                if (i < rating) {
+                    ratingString += "<li class='c-rating__item--starred'></li>";
+                } else {
+                    ratingString += "<li class='c-rating__item'></li>";
+                }
+            }
+                
+            // determine what should go below the image
+            if (movie.rating){
+                actionDiv = `<div class="card-action" id="movie-action!${c}">
+                        <ul class="c-rating">
+                            ${ratingString}
+                        </ul>
+                    </div>`;
+            } else if (movie.watchlist) {
+                actionDiv = `<div class="card-action" id="movie-action!${c}">
+                    <a href="#">Watched?</a>
+                    </div>`;
+            } else {
+                actionDiv = `<div class="card-action" id="movie-action!${c}">
+                    <a href="#">Add To Watchlist</a>
+                    </div>`;
+            }
+
+            // put the pieces together
+            const posterPath = `http://image.tmdb.org/t/p/w300${movie.posterPath}`;
+            
+            cardContainer.innerHTML +=
+                        `<div class="card" id="movie!${c}">
                             <div class="card-image">
                                 <img src="${posterPath}">
                                 <span class="card-title">${movie.name}</span>
                             </div>
                             <div class="card-content">
-                            <p>${movie.overview}</p>
+                            <p>${movie.overview.substring(0,30)}</p>
                             </div>
-                            <div class="card-action">
-                            <a href="#">This is a link</a>
-                            </div>
-                        </div>
-                    </div>`;
-            });
-            appendToElement.append(row);
+                            ${actionDiv}
+                        </div>`;
+
+            appendToElement.append(cardContainer);
 
         },
         enumerable: true
     },
+
     "trackedToWatched": {
         value: function(elementId) {
 
@@ -107,5 +69,61 @@ const Renderer = Object.create(null, {
     }
 });
 
-Renderer.all(data,$(".article__image"));
+module.exports = Renderer;
 
+//Test Data
+// const data = [{
+//     "name": "Bourne Identify",
+//     "releaseDate": "2001-12-01",
+//     "actors": null,
+//     "id": 2501,
+//     "watchlist": false,
+//     "watched": false,
+//     "rating": null,
+//     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+//     "overview": "In a world..."
+// },
+// {
+//     "name": "Bourne Identify",
+//     "releaseDate": "2001-12-01",
+//     "actors": null,
+//     "id": 2501,
+//     "watchlist": true,
+//     "watched": false,
+//     "rating": null, 
+//     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+//     "overview": "In a world..."
+// },
+// {
+//     "name": "Bourne Identify",
+//     "releaseDate": "2001-12-01",
+//     "actors": null,
+//     "id": 2501,
+//     "watchlist": true,
+//     "watched": true,
+//     "rating": 2,
+//     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+//     "overview": "In a world..."
+// },
+// {
+//     "name": "Bourne Identify",
+//     "releaseDate": "2001-12-01",
+//     "actors": null,
+//     "id": 2501,
+//     "watchlist": true,
+//     "watched": true,
+//     "rating": 3,
+//     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+//     "overview": "In a world..."
+// },
+// {
+//     "name": "Bourne Identify",
+//     "releaseDate": "2001-12-01",
+//     "actors": null,
+//     "id": 2501,
+//     "watchlist": true,
+//     "watched": true,
+//     "rating": 5,
+//     "posterPath": "/bXQIL36VQdzJ69lcjQR1WQzJqQR.jpg",
+//     "overview": "In a world..."
+// }];

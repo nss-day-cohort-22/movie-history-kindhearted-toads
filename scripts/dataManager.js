da4c9f5ddd8a37c0f1e92828a99f78fdcd652892
Christopher Miller <camiller.yr@gmail.com>
2017-11-28 13:49:02 -0600

    data handler object

---
+++ scripts/dataManager.js
@@ -0,0 +1,100 @@
+// Data Handler - Chris Miller
+// Responsible for creating and returning all ajax calls
+
+const $ = require("jQuery")
+const auth = require("../authentication/auth")
+
+// MovieDB.searchMovie({ query: "Alien" }, (err, res) => {
+//     console.log(res)
+// });
+
+const dataManager = Object.create(null, {
+    "firebaseGET": {
+        "value": function () {
+            return firebase.auth().currentUser.getToken(true)
+                .then(idToken => {
+                    return $.ajax({
+                        "url": `https://movie-history-59344.firebaseio.com/${auth.activeUser.uid}/.json?auth=${idToken}`,
+                        "method": "GET",
+                    })
+                })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "firebasePOST": {
+        "value": function (newObject) {
+            return firebase.auth().currentUser.getToken(true)
+                .then(idToken => {
+                    return $.ajax({
+                        "url": `https://movie-history-59344.firebaseio.com/${auth.activeUser.uid}/.json?auth=${idToken}`,
+                        "method": "GET",
+                        "data": JSON.stringify(newObject)
+                    })
+                })
+        }, "writable": true, "enumerable": true
+    },
+
+    "firebaseDELETE": {
+        "value": function (objectID) {
+            return firebase.auth().currentUser.getToken(true)
+                .then(idToken => {
+                    return $.ajax({
+                        "url": `https://movie-history-59344.firebaseio.com/${auth.activeUser.uid}/${objectID}.json?auth=${idToken}`,
+                        "method": "DELETE",
+                    })
+                })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "firebasePUT": {
+        "value": function(editedObject) {
+            return firebase.auth().currentUser.getToken(true)
+                .then(idToken => {
+                    return $.ajax({
+                        "url": `https://movie-history-59344.firebaseio.com/${auth.activeUser.uid}/${editedObject.id}.json?auth=${idToken}`,
+                        "method": "PUT",
+                        "data": JSON.stringify(editedObject.value)
+                    })
+                })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "getMovieByID": {
+        "value": function(movieID) {
+            return $.ajax({
+                "url": `https://api.themoviedb.org/3/movie/${movieID}?api_key=feae42ac1de7d9384c48dcb8682cb73f&append_to_response=credits`,
+                "method": "GET"
+            })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "searchMovies": {
+        "value": function(searchString) {
+            return $.ajax({
+                "url": `https://api.themoviedb.org/3/search/movie?api_key=feae42ac1de7d9384c48dcb8682cb73f&language=en-US&query=${searchString}&page=1`,
+                "method": "GET"
+            })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "getCast": {
+        "value": function(movieID) {
+            return $.ajax({
+                "url": `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=feae42ac1de7d9384c48dcb8682cb73f
+                `,
+                "method": "GET"
+            })
+        }, "writable": true, "enumerable": true
+    },
+    
+    "getPopular": {
+        "value": function() {
+            return $.ajax({
+                "url": "https://api.themoviedb.org/3/movie/popular?api_key=feae42ac1de7d9384c48dcb8682cb73f&language=en-US&page=1",
+                "method": "GET"
+            })
+        }, "writable": true, "enumerable": true
+    },
+})
+
+module.exports = dataManager

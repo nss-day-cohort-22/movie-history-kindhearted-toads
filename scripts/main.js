36e05b9c9f488e5108d7bf0ff4d2fc06439440e7
Greg Lawrence <gregaudio@gmail.com>
2017-11-30 09:06:23 -0600

    moved eventListener creation from observer.js to main.js to stop the creation of duplicate event listeners

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,12 @@ const auth = require("./authentication/auth")
 const $ = require("jquery")
 require("materialize")
 
+const addListenersSearch = require("./addListenersSearch")
+const addListenersTrackedMovies = require("./trackedMovies/addListenersTrackedMovies")
+const addListenersCard = require("./addListenersCards");
+
 auth.init()
+
+addListenersSearch()
+addListenersTrackedMovies()
+addListenersCard()

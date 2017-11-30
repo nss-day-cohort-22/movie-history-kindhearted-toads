69e1c2ac2bf0cf4cc931b0c169ccf200ec5fd802
Christopher Miller <camiller.yr@gmail.com>
2017-11-30 11:43:26 -0600

    set timeout

--- scripts/addListenersSearch.js
+++ scripts/addListenersSearch.js
@@ -13,8 +13,7 @@ const addListenersSearch = function() {
 
         
         // get value from search field
-        let searchfield = $("#searchField")
-        let searchQuery = searchfield.val()
+        let searchQuery = $("#searchField").val()
         
         
         // check if the button clicked was Find A Movie
@@ -47,8 +46,6 @@ const addListenersSearch = function() {
                     if ($(".movieResults__cardContainer").html() === "") {
                         $(".movieResults__cardContainer").html("<h5>No matches found</h5>")
                     }
-
-                    searchfield.val("")
                 })
                 
             } else {
@@ -77,7 +74,6 @@ const addListenersSearch = function() {
                         renderer.append(movie, "trackedMovies__cardContainer")
                     }
 
-                    searchfield.val("")
                 })
             } else {
                 // if nothing is in search input field and user clicks "Search My Movies", show ALL of their tracked movies
